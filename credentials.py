b0f12e995866b74a9a55f57aafe12a9d6557a052
Mazawrath <Mazawrath@users.noreply.github.com>
2019-04-24 02:34:21 -0400

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Create an application on https://developer.twitter.com/ and Create a Twitter application and 
+# generate a Consumer Key, Consumer Secret, Access Token, and Access Token Secret. 
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+# (optional) Create an account on https://sentry.io for error tracking
+dsn = ''
