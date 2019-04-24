3bbe33f389a838823e9f929ea47a754a69162ebb
Mazawrath <Mazawrath@users.noreply.github.com>
2019-04-24 14:02:20 -0400

    Improved listener

--- listener.py
+++ listener.py
@@ -1,5 +1,6 @@
 from utility import get_api
 import tweepy
+import re
 
 api = get_api()
 
@@ -17,10 +18,15 @@ follower_list = get_follower_list()
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        print(status.text)
+        if status.user.id_str in follower_list:
+            # Retweet
+            if re.match(r'^RT', status.text, flags=0):
+                print('Retweet from ' + status.user.screen_name)
+            else:
+                print(status.text)
 
 
 myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener, stall_warnings=True, tweet_mode='extended')
 
 myStream.filter(follow=follower_list)
