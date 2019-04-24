7ef68b02b89057152de584b2f7dcc759170b35a8
Mazawrath <Mazawrath@users.noreply.github.com>
2019-04-24 14:03:21 -0400

    Delete credentials.py

--- credentials.py
+++
@@ -1,9 +0,0 @@
-# Create an application on https://developer.twitter.com/ and Create a Twitter application and 
-# generate a Consumer Key, Consumer Secret, Access Token, and Access Token Secret. 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
-
-# (optional) Create an account on https://sentry.io for error tracking
-dsn = ''
