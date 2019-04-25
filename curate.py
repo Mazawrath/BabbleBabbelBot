9640c26e8609395eeaa0439c147b6a4670bc3966
Mazawrath <Mazawrath@users.noreply.github.com>
2019-04-25 00:00:40 -0400

    Impoved curator

--- curate.py
+++ curate.py
@@ -6,8 +6,8 @@ file_list = os.listdir(file_path + '/pending')
 
 for single_tweet in file_list:
     pending_tweet = open(file_path + 'pending/' + single_tweet, 'r+')
-    screen_name = pending_tweet.readline()
-    tweet_time = pending_tweet.readline()
+    screen_name = pending_tweet.readline().rstrip()
+    tweet_time = pending_tweet.readline().rstrip()
     tweet = ''
     for line in pending_tweet:
         tweet = tweet + line
@@ -21,6 +21,11 @@ for single_tweet in file_list:
     if keyboard == 'y':
         while True:
             translated_tweet = translate.get_translated_tweet(tweet)
+            tweet_over = 250 - (2 + len(screen_name)) - len(translated_tweet)
+            if tweet_over < 0:
+                print('Tweet may be cut off.')
+                translated_tweet = translated_tweet[:tweet_over]
+            translated_tweet = translated_tweet + '\n@' + screen_name
             print(translated_tweet)
             keyboard = input('y: Accept tweet\n'
                              'n: Translate again\n'
