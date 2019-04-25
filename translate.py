fadcd2a62e7f68b787a8f605d0dc94e3b1a5036e
Mazawrath <Mazawrath@users.noreply.github.com>
2019-04-24 20:37:11 -0400

    Added new translate languages and imporved tweet translation

--- translate.py
+++ translate.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
+from credentials import key_1
 import requests
 import uuid
-import json
-import credentials
+import random
 
-# If you want to set your subscription key as a string, uncomment the line
-# below and add your subscription key.
-subscriptionKey = credentials.key_1
+subscriptionKey = key_1
 
 base_url = 'https://api.cognitive.microsofttranslator.com'
 path = '/translate?api-version=3.0'
@@ -22,21 +20,57 @@ def get_language_dir(value):
     # English
     if value == 0:
         return 'en'
-    # Spanish
+    # Afrikaans
     elif value == 1:
-        return 'es'
-    # Russian
+        return 'af'
+    # Arabic
     elif value == 2:
-        return 'ru'
-    # Portuguese
+        return 'ar'
+    # Bangla
     elif value == 3:
-        return 'pt'
-    # Polish
+        return 'bn'
+    # Bosnian (Latin)
     elif value == 4:
-        return 'pl'
-    # Norwegian
+        return 'bs'
+    # Bulgarian
     elif value == 5:
-        return 'nb'
+        return 'bg'
+    # Cantonese (Traditional)
+    elif value == 6:
+        return 'yue'
+    # Catalan
+    elif value == 7:
+        return 'ca'
+    # Chinese Simplified
+    elif value == 8:
+        return 'zh-Hans'
+    # Chinese Traditional
+    elif value == 9:
+        return 'zh-Hant'
+    # Croatian
+    elif value == 10:
+        return 'hr'
+    # Czech
+    elif value == 11:
+        return 'cs'
+    # Danish
+    elif value == 12:
+        return 'da'
+    # Dutch
+    elif value == 13:
+        return 'nl'
+    # Estonian
+    elif value == 14:
+        return 'et'
+    # Fijian
+    elif value == 15:
+        return 'fj'
+    # Filipino
+    elif value == 16:
+        return 'fil'
+    # Finnish
+    elif value == 17:
+        return 'fi'
 
 
 def translate_text(language_to, translate_text):
@@ -48,9 +82,24 @@ def translate_text(language_to, translate_text):
     }]
 
     request = requests.post(constructed_url, headers=headers, json=body)
-    response = request.json()
-
-    print(json.dumps(response, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
+    return request.json()[0]['translations'][0]['text']
 
 
-print(translate_text(get_language_dir(3), 'Hello world, how are you doing?'))
+def generate_random_nums(size):
+    while True:
+        retval = random.sample(range(0, 17), size)
+        # If there are no duplicates in the array and the first/last language isn't english, break
+        if len(retval) == len(set(retval)) and not retval[0] == 0 and not retval[size - 1] == 0:
+            break
+    return retval
+
+
+def get_translated_tweet(text):
+    output = text
+    # Generate numbers to translate to each language
+    translate_nums = generate_random_nums(10)
+    for i in range(len(translate_nums)):
+        output = translate_text(get_language_dir(translate_nums[i]), output)
+    # Translate back to English
+    output = translate_text(get_language_dir(0), output)
+    return output
