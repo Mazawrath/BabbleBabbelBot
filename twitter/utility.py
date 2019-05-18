ba155b76501aea00620e7ad43f42ebfd87e19d3d
Mazawrath <Mazawrath@users.noreply.github.com>
2019-05-18 16:11:54 -0400

    Renamed sentry dsn variable name

--- twitter/utility.py
+++ twitter/utility.py
@@ -1,10 +1,10 @@
-from credentials import dsn, consumer_key, consumer_secret, access_token, access_token_secret
+from credentials import twitter_dsn, consumer_key, consumer_secret, access_token, access_token_secret
 import tweepy
 import sentry_sdk
 
 
 def authenticate_twitter():
-    sentry_sdk.init(dsn)
+    sentry_sdk.init(twitter_dsn)
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
